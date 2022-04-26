ddb79210ad913aeb06c142637e8cb323c6609567
Amaderu <ilahavelev454@gmail.com>
2022-04-26 13:45:42 +0700

    added files to track

---
+++ src/main/java/com/amaderu/SpringArtifact/SpringSeviceApplication.java
@@ -0,0 +1,13 @@
+package com.amaderu.SpringArtifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSeviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSeviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amaderu/SpringArtifact/SpringSeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amaderu.SpringArtifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSeviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
