fc6679181c2d6161a1872676c9791e82d9f5914d
Amaderu <ilahavelev454@gmail.com>
2022-07-31 18:39:29 +0700

    fixes

--- spring-security-client/src/main/java/com/amaderu/client/entity/PasswordResetToken.java
+++ spring-security-client/src/main/java/com/amaderu/client/entity/PasswordResetToken.java
@@ -15,6 +15,38 @@ public class PasswordResetToken {
 
     private Date expirationTime;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Date getExpirationTime() {
+        return expirationTime;
+    }
+
+    public void setExpirationTime(Date expirationTime) {
+        this.expirationTime = expirationTime;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
     @OneToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id", referencedColumnName = "id",
             nullable = false,
